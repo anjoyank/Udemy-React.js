6d6b41fc50c4f434ef8b2d24f8a907b664b3ef32
Andrew Yankee <anjoyank@gmail.com>
2021-06-15 09:00:13 -0400

    stuff

--- FoodOrderApp/FoodOrderApp/src/components/Meals/MealItem/MealItemForm.js
+++ FoodOrderApp/FoodOrderApp/src/components/Meals/MealItem/MealItemForm.js
@@ -1,9 +1,11 @@
-import {useRef} from 'react';
+import {useRef, useState} from 'react';
 
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+
+  const [amountIsValid, setAmountIsValid] = useState(true);
 
   const amountInputRef = useRef();
 
@@ -11,6 +13,14 @@ const MealItemForm = (props) => {
     event.preventDefault();
 
     const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber);
   };
 
   return (
@@ -28,6 +38,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
     </form>
   );
 };
